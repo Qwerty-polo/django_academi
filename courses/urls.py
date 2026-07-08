6ef9c2f2002bc06b5c1d92f9b2cb832bacab6511
Max <Max@gmail.com>
2026-07-08 10:00:41 +0300

    initial commit

---
+++ courses/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('add-course/', views.AddCourseView.as_view(), name='add-course'),
+
+    path('tarrifs/', views.tarrifsPage, name='tarrifs'),
+
+    path('course/<slug>',views.CourseDetailPage.as_view(), name='course-detail'),
+    path('course/<slug>/<lesson_slug>', views.LessonDetailPage.as_view(), name='lesson-detail'),
+
+]
---
+++ users/views.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm, ProfileImageForm, UserUpdateForm
+from django.contrib import messages
+from django.contrib.auth import logout
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'user {username} was created')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request,
+                  'users/registration.html',
+                  {
+                      'title': 'Сторінка реєстрації',
+                      'form': form,
+                   })
+
+def custom_logout(request):
+  logout(request)
+  # Повідомлення тепер не потрібне, бо вся інформація буде на самій сторінці
+  return render(request, 'users/exit.html')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        profileForm = ProfileImageForm(request.POST, request.FILES,instance=request.user.profile)
+        updateUserForm = UserUpdateForm(request.POST, instance=request.user)
+
+        if profileForm.is_valid() and updateUserForm.is_valid():
+            profileForm.save()
+            updateUserForm.save()
+            messages.success(request, f'your profile was updated')
+            return redirect('profile')
+
+    else:
+        profileForm = ProfileImageForm(instance=request.user.profile)
+        updateUserForm = UserUpdateForm(instance=request.user)
+
+
+    data = {
+        'profileForm': profileForm,
+        'updateUserForm': updateUserForm
+    }
+
+    return render(request, 'users/profile.html', data)
