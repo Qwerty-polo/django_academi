cc3d507fe8335135f5d63b3d0a7aaf36bb99340f
Maks <msmaks52@gmail.com>
2026-07-27 19:40:35 +0300

    Refactor Course, Lesson, Profile models and add Pillow dependency

--- courses/models.py
+++ courses/models.py
@@ -4,27 +4,33 @@ from django.urls import reverse
 
 
 class Course(models.Model):
-    #slug, title, desc, image
-    slug = models.SlugField('Unique name of course',unique=True)
-    title = models.CharField('Name of course',max_length=100)
+    slug = models.SlugField('Unique name of course', unique=True)
+    title = models.CharField('Name of course', max_length=100)
     desc = models.TextField('Description of course')
-    image = models.ImageField('Image', default='default.png',upload_to='courses_images')
-    is_free = models.BooleanField('Free',default=True)
+    image = models.ImageField('Image', default='default.png', upload_to='courses_images')
+    is_free = models.BooleanField('Free', default=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('course-detail', kwargs={'slug':self.slug})
+        return reverse('course-detail', kwargs={'slug': self.slug})
+
 
 class Lesson(models.Model):
-    #slug, title, desc, course, number, video_url
-    slug = models.SlugField('Unique name of lesson',unique=True)
-    title = models.CharField('Name of lesson',max_length=100)
+    slug = models.SlugField('Slug of lesson')
+    title = models.CharField('Name of lesson', max_length=100)
     desc = models.TextField('Description of lesson')
-    course = models.ForeignKey(Course,on_delete=models.CASCADE, verbose_name='which course?')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Which course?')
     number = models.IntegerField('Lesson number')
-    video = models.CharField('Video',max_length=100)
+    video = models.URLField('Video URL', max_length=200)
+
+    class Meta:
+        ordering = ['number']
+        unique_together = ('course', 'slug')
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
 
     def get_absolute_url(self):
         return reverse('lesson-detail', kwargs={
@@ -32,13 +38,15 @@ class Lesson(models.Model):
             'lesson_slug': self.slug
         })
 
-    def __str__(self):
-        return self.title
-    #
-    # def get_absolute_url(self):
-    #     return reverse('lesson-detail', kwargs={'slug':self.course.slug, 'lesson_slug':self.slug})
 
 class Comment(models.Model):
     text = models.TextField('Comment', max_length=200)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']  
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.lesson.title}"
--- users/models.py
+++ users/models.py
@@ -1,38 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-TYPE_ACCOUNT = (
-    ('full','full mode'),
-    ('free', 'free mode'),
 
+TYPE_ACCOUNT = (
+    ('full', 'full mode'),
+    ('free', 'free mode'),
 )
 
+GENDER_CHOICES = (
+    ('male', 'Man'),
+    ('female', 'Woman'),
+)
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img = models.ImageField('user photo', default = 'default.png', upload_to='user_images')
-    account_type = models.CharField(choices=TYPE_ACCOUNT, max_length=30, default='free')
-
-    GENDER_CHOICES = (
-        ('male', 'man'),
-        ('female', 'women'),
-    )
-    gender = models.CharField('sex', max_length=10, choices=GENDER_CHOICES, blank=True)
-    email_consent = models.BooleanField('agree on get an email',default=True)
-
-
-    def __str__(self):
-        return f'Профайл юзера {self.user.username}'
-
-    def save(self, *args, **kwargs):
-        super().save()
-
-        image = Image.open(self.img.path)
-
-        if image.height > 256 or image.width > 256:
-            resize = (256, 256)
-            image.thumbnail(resize)
-            image.save(self.img.path)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    img = models.ImageField('User Photo', default='default.png', upload_to='user_images')
+    account_type = models.CharField('Account Type', choices=TYPE_ACCOUNT, max_length=30, default='free')
+    gender = models.CharField('Sex', max_length=10, choices=GENDER_CHOICES, blank=True)
+    email_consent = models.BooleanField('Agree to receive emails', default=True)
 
     class Meta:
-        verbose_name='Профайл'
-        verbose_name_plural = 'Профайли'
+        verbose_name = 'Профіль'
+        verbose_name_plural = 'Профілі'
+
+    def __str__(self):
+        return f'Профіль користувача {self.user.username}'
+
+    @property
+    def is_vip(self):
+        return self.account_type == 'full'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+        try:
+            image = Image.open(self.img.path)
+            if image.height > 256 or image.width > 256:
+                resize = (256, 256)
+                image.thumbnail(resize)
+                image.save(self.img.path)
+        except Exception:
+            pass
